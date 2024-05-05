7510505fa8b11fdd6078092894dd535f40d3ba9d
Omar Elzr <75176072+omaribrahim12@users.noreply.github.com>
2024-05-05 12:13:21 +0100

    Update spotify_setup.py

--- spotify_setup.py
+++ spotify_setup.py
@@ -3,7 +3,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 def setup_spotify_client():
     # Set up Spotify client credentials
-    client_id = 'bfd4dd86e7294beba632b058f681bce8'
-    client_secret = 'bcd8f89b80f14c34a7f9473c2de5d151'
+    client_id = 'ENTER_YOUR_CLIENT_ID'
+    client_secret = 'ENTER_YOUR_CLIENT_SECRET'
     client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
     return spotipy.Spotify(client_credentials_manager=client_credentials_manager)
